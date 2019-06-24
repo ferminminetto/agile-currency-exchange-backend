022575d50fb76bfbac49c152a6952807a486c184
Fermín Minetto <fermin.minetto@gmail.com>
2019-06-24 20:44:57 -0300

    feature - Add Seeds

--- common/management/commands/development_seed.py
+++ common/management/commands/development_seed.py
@@ -2,30 +2,42 @@ from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
 from agile_currency_exchange.settings import BASE_DIR
 from currencies.models import Currency
+from accounts.models import Account
 import json
 
 
 def test_user_seed():
     User.objects.create_superuser('test', 'test@test.com', 'test')
+    User.objects.create_user('antonio', 'antonio@test.com', 'antonio')
+    User.objects.create_user('fermin', 'fermin@test.com', 'fermin')
+
+    Account.objects.create(
+        balance=60000.0, owner=User.objects.get(username='antonio'), currency=Currency.objects.get(code='JPY')
+    )
+    Account.objects.create(
+        balance=30000.0, owner=User.objects.get(username='fermin'), currency=Currency.objects.get(code='ARS')
+    )
 
 
 def currencies_seeder(path_to_open):
     currencies_data = json.load(open(path_to_open))
     for currency_data_index in currencies_data:
         currency_data = currencies_data[currency_data_index]
-        is_master = False if currency_data['code'] != 'USD' else True
+        code = currency_data['code']
+        is_master = False if code != 'USD' else True
+        active = True if code == 'USD' or code == 'ARS' or code == 'JPY' else True
         Currency.objects.create(
             symbol=currency_data['symbol'], code=currency_data['code'], name=currency_data['name'],
             name_plural=currency_data['name_plural'], decimal_digits=currency_data['decimal_digits'],
-            is_master=is_master, active=is_master
+            is_master=is_master, active=active
         )
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        test_user_seed()
-        print('Test User created successfully!')
-
         path_to_open = BASE_DIR + '/common/management/commands/currencies.json'
         currencies_seeder(path_to_open)
+
+        test_user_seed()
+        print('Test Users created successfully!')
