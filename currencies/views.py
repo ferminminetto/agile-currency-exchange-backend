1f3cc6d188a1d5652630077c5fe2c373483e3294
Fermín Minetto <fermin.minetto@gmail.com>
2019-06-25 23:29:34 -0300

    #17 - Expose Available Currencies

--- currencies/views.py
+++ currencies/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
 
 from django.conf import settings
@@ -13,7 +14,8 @@ CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 class CurrencyViewSet(viewsets.ModelViewSet):
 
-    queryset = Currency.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Currency.objects.all().filter(active=True)
     serializer_class = CurrencySerializer
 
     @method_decorator(cache_page(CACHE_TTL))
